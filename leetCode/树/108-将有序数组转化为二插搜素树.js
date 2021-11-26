0659b17a0efb43d47f2115b817d2b11f2999bd0f
10swan <1259496020@qq.com>
2021-11-26 12:51:26 +0800

    108

--- leetCode/树/108-将有序数组转化为二插搜素树.js
+++ leetCode/树/108-将有序数组转化为二插搜素树.js
@@ -10,19 +10,20 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function(nums) {
-    if (nums.length === 0) {
-        return null;
-    }
-    const dfs = function(nums, left, right) {
-        if (left > right) {
-            return null;
-        }
-        let mid = math.floor(left + (right - left) / 2);
-        let root = new TreeNode(nums[mid]);
-        root.left = dfs(nums, left, mid - 1);
-        root.right = dfs(nums, mid + 1, right);
-        return root;
-    }
-    return dfs(nums, 0, nums.length - 1);
+var sortedArrayToBST = function (nums) {
+  if (nums.length === 0) return null;
+  const dfs = function (nums, left, right) {
+    // 限定范围
+    if (left > right) return null;
+    // 找数组中点
+    let mid = Math.floor(left + (right - left) / 2);
+    // 创建根结点
+    let root = new TreeNode(nums[mid]);
+    // 左子树
+    root.left = dfs(nums, left, mid - 1);
+    // 右子树
+    root.right = dfs(nums, mid + 1, right);
+    return root;
+  }
+  return dfs(nums, 0, nums.length - 1);
 };
