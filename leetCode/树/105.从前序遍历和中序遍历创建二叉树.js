1370b0c0307b018e8c8005e23220a806e00a88aa
10swan <1259496020@qq.com>
2021-11-26 14:19:14 +0800

    优化

--- leetCode/树/105.从前序遍历和中序遍历创建二叉树.js
+++ leetCode/树/105.从前序遍历和中序遍历创建二叉树.js
@@ -11,6 +11,7 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
+// 基础解法
 var buildTree = (preorder, inorder) => {
   if (preorder.length == 0) return null;
   // 根结点是先根遍历的第一个结点
@@ -24,3 +25,22 @@ var buildTree = (preorder, inorder) => {
   // 返回根结点
   return root;
 };
+// 时间和空间优化
+// const buildTree = (preorder, inorder) => {
+//   const map = new Map();
+//   for (let i = 0; i < inorder.length; i++) {
+//     map.set(inorder[i], i);
+//   }
+//   const helper = (p_start, p_end, i_start, i_end) => {
+//     if (p_start > p_end) return null;
+//     let rootVal = preorder[p_start];    // 根节点的值
+//     let root = new TreeNode(rootVal);   // 根节点
+//     let mid = map.get(rootVal);         // 根节点在inorder的位置
+//     let leftNum = mid - i_start;        // 左子树的节点数
+//     root.left = helper(p_start + 1, p_start + leftNum, i_start, mid - 1);
+//     root.right = helper(p_start + leftNum + 1, p_end, mid + 1, i_end);
+//     return root;
+//   };
+//   return helper(0, preorder.length - 1, 0, inorder.length - 1);
+// };
+
