c38052e68a5ba6735701fdbb249c45b8b3d56a56
10swan <1259496020@qq.com>
2021-11-29 16:10:03 +0800

    call-apply-bind

---
+++ js/call-apply-bind/apply.js
@@ -0,0 +1,32 @@
+var b = {
+  name: 'aa'
+}
+function a(e, ee) {
+  console.log(this.name);
+  return e + ee;
+}
+// a.apply(b, [1, 2]);
+
+Function.prototype.myapply = function (context) {
+
+  if (typeof this !== 'function') {
+
+    throw new TypeError('Error');
+  }
+
+  context = context || window;
+
+  context.fn = this;
+
+  //  和apply的区别是只有一个参数，所以取出第二个参数就可以了(第二个参数是数组，需要将数组结构为一个个元素)
+  let result = context.fn(...arguments[1]);
+
+  delete context.fn;
+
+  return result;
+}
+
+// 可以有返回值也可以没有返回值
+let c = a.myapply(b, [1, 2]);
+console.log(c, '-----------');//有返回值
+a.myapply(b, [1, 2]);//无返回值
--- js/call-apply-bind/call.js
+++ js/call-apply-bind/call.js
@@ -4,17 +4,18 @@
 var b = {
   name: 'aa'
 }
-function a() {
+function a(e, ee) {
   console.log(this.name);
+  console.log(e + ee);
 }
 
-a.call(b);//相当于b.a(),如下
-var b = {
-  name: 'aa',
-  // a: function () {
-  //   console.log(this.name);
-  // }
-}
+// a.call(b);//相当于b.a(),如下
+// var b = {
+//   name: 'aa',
+//   // a: function () {
+//   //   console.log(this.name);
+//   // }
+// }
 Function.prototype.mycall = function (context) {
   // 调用call的是this，所以我们可以通过判断this类型来判断是否是函数
   if (typeof this !== 'function') {
@@ -23,11 +24,16 @@ Function.prototype.mycall = function (context) {
   }
   // context存在则为context否则为window
   context = context || window;
-
+  // 获取第一个参数后面所有参数
+  const args = [...arguments].slice(1);
   // 把调用我的那个函数获取到，复制到context对象中去
   context.fn = this;// b.fn = a;相当于往b对象中添加键值对属性fn:a()
 
-  context.fn();// b.a()
+  let result = context.fn(...args);// b.a()
 
   delete context.fn;// 用完删除防止属性修改
+
+  return result;
 }
+
+a.mycall(b, 1, 2);
