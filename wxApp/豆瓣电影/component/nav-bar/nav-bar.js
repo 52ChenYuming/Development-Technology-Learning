9a5a1fc8c6dbcebed3fd0c7cc406d9368f32fd33
10swan <1259496020@qq.com>
2021-11-30 21:52:22 +0800

    douban movies

--- wxApp/豆瓣电影/component/nav-bar/nav-bar.js
+++ wxApp/豆瓣电影/component/nav-bar/nav-bar.js
@@ -6,8 +6,22 @@ Component({
   properties: {
     statusBarColor:{
       type:String,
+      value:'#fff',
+      navBarStyle:''
+    },
+    navBarColor:{
+      type:String,
       value:'#fff'
+    },
+    titleColor:{
+      type:String,
+      value:'#000'
+    },
+    title:{
+      type:String,
+      value:''
     }
+
   },
 
   /**
@@ -15,19 +29,25 @@ Component({
    */
   data: {
     topHeight:0,
-    statusBarStyle:''
+    statusBarStyle:'',
+    navBarStyle:''
   },
   lifetimes:{
     attached:function(){
       let statusBarStyle = `background-color :${this.data.statusBarColor};
       height:${wx.db.statusBarHeight}px`;
-      console.log(statusBarStyle);
+      
+      let navBarStyle = `background-color :${this.data.navBarColor};
+      height:${wx.db.navBarHeight}px;
+      color:${this.data.titleColor}
+      `;
 
       let topHeight = wx.db.navBarHeight + wx.db.statusBarHeight;
       // console.log(topHeight);
       this.setData({
         topHeight,
-        statusBarStyle
+        statusBarStyle,
+        navBarStyle,
       })
     }
   },
