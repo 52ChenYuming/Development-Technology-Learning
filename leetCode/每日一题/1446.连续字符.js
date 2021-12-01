c23f40c0fad844cfa536e9c32c009598b9bb25a9
10swan <1259496020@qq.com>
2021-12-01 11:31:37 +0800

    1446

---
+++ leetCode/每日一题/1446.连续字符.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function (s) {
+  let max = 1, count = 1;
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      count++;
+      max = Math.max(max, count);
+    } else count = 1;
+  }
+  return max
+};
