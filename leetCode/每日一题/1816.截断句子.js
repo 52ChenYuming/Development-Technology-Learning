a9b1f81b4bda4747f5f106f65fad501ef7a6adb0
10swan <1259496020@qq.com>
2021-12-06 15:30:05 +0800

    1816

--- leetCode/每日一题/1816.截断句子.js
+++ leetCode/每日一题/1816.截断句子.js
@@ -3,15 +3,34 @@
  * @param {number} k
  * @return {string}
  */
-var truncateSentence = function (s, k) {
-  let arr = s.split(" ");
-  console.log(arr);
-  for (let i = k; i < arr.length; k++) {
-    arr.pop();
-    console.log(arr);
-  }
+// var truncateSentence = function (s, k) {
+//   let arr = s.split(" ");
+//   console.log(arr);
+//   for (let i = k; i < arr.length; k++) {
+//     arr.pop();
+//     console.log(arr);
+//   }
 
-  return arr.join(" ");
+//   return arr.join(" ");
+// };
+
+var truncateSentence = function (s, k) {
+  // 终止点
+  let end = 0;
+  // 字符计数
+  let count = 0;
+  for (let i = 1; i <= s.length; i++) {
+    // 如果遇到空格或者最后一个字符，count++
+    if (s[i] == ' ' || i == s.length) {
+      count++;
+      // 如果前k个字符遍历完毕，结束
+      if (count == k) {
+        end = i;
+        break;
+      }
+    }
+  }
+  return s.slice(0, end);
 };
 
-truncateSentence("What is the solution to this problem", 4);
+truncateSentence("chopper is not a tanuki", 5);
