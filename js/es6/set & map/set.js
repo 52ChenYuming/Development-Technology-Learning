a61974bca24ee272784311643a286f3dd326b9b1
10swan <1259496020@qq.com>
2021-12-13 19:19:09 +0800

    set&map

---
+++ js/es6/set & map/set.js
@@ -0,0 +1,7 @@
+
+let s = new Set([1, 2, 3, 4, 5, 5, 5, 5, 5])
+console.log(s);
+s.add(6);
+console.log(s);
+s.delete(6);
+console.log(s);
---
+++ js/es6/set & map/weakSet.js
@@ -0,0 +1,2 @@
+const ws = new WeakSet([1, 2], [3, 4])
+console.log(ws);
