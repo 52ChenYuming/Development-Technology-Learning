b01655bfff476ebc4efe994fc6f7fb89afa26d5d
10swan <1259496020@qq.com>
2021-12-14 16:56:50 +0800

    set&map

--- js/es6/set & map/set.js
+++ js/es6/set & map/set.js
@@ -1,7 +1,54 @@
+// const s  = new Set()
 
-let s = new Set([1, 2, 3, 4, 5, 5, 5, 5, 5])
-console.log(s);
-s.add(6);
-console.log(s);
-s.delete(6);
-console.log(s);
+// // console.log(s);
+
+// Array.from([2, 3, 4, 5, 2, 2]).forEach(x => {
+//   s.add(x)
+// });
+// // console.log(s);
+
+// let arr = [1, 2, 3, 2, 1]
+// let newArr = new Set(arr)
+// console.log(newArr);
+
+
+// let s = new Set([1, 2, 3, 4, 5, 5, 5]) // {1: 1, 2: 2, 3: 3, ...}
+// s.add(6)
+// s.delete(5)   // delete obj.name
+// s.clear()
+// let arr = ['a', 'b', 'c']
+
+// console.log(s.entries());
+
+// for (let item of s.entries()) {
+//   console.log(item);
+// }
+
+// s.forEach((value, key) => {
+//   console.log(value, key);
+// })
+// let newS = [...s]
+// // newS.map((x) => {
+// //   console.log(x);
+// // })
+// let biger = newS.filter((x) => {
+//   return x > 2
+// })
+// console.log(biger);
+
+
+
+let a = new Set([1, 2, 3])
+let b = new Set([4, 3, 2])
+
+let union = new Set([...a, ...b])
+let intersect = new Set([...a].filter(x => b.has(x)))
+let differnce = new Set([...a].filter(x => !b.has(x)))
+
+
+let newA = [...a].map(val => val + 1)
+for (let i of a.values()) {
+  i = i + 1
+}
+
+console.log(a);
--- js/es6/set & map/weakSet.js
+++ js/es6/set & map/weakSet.js
@@ -1,2 +1,32 @@
-const ws = new WeakSet([1, 2], [3, 4])
+// const ws = new WeakSet()
+// ws.add(1)
+
+// console.log(ws);
+
+
+
+
+// let a = [[1, 2]]
+// function foo() {
+//   let ws = new WeakSet(a)
+//   // .....
+//   return ws
+// }
+
+// let bar = foo()
+// a = null
+// console.log(bar);
+
+
+
+let a = 1
+let arr = []
+arr.push(a)
+a = null
+console.log(arr);
+
+
+let b = [[1, 2]]
+let ws = new WeakSet(b)
+b = null
 console.log(ws);
