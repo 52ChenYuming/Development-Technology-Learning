7e4fdc8d808c306d214d1c86537e1cad3d8ef58d
10swan <1259496020@qq.com>
2021-12-19 19:51:16 +0800

    我的博客

--- node/Bolg/routes/category.js
+++ node/Bolg/routes/category.js
@@ -2,10 +2,63 @@ const CategoryModel = require('../models/category')
 
 module.exports = {
   async list(ctx, next) {
-    const categories = await CategoryModel.find({});
+    const categories = await CategoryModel.find({})
     await ctx.render('category', {
       title: '新建分类',
       categories
     })
+  },
+  async newCategory(ctx, next) {
+    if (ctx.method === 'GET') {
+      await ctx.render('create_category', {
+        title: '新建分类'
+      })
+    } else {
+      const { name, title, desc } = ctx.request.body
+      let errMsg = ''
+      if (name === '') {
+        errMsg = '分类名不能为空'
+      } else if (title === '') {
+        errMsg = '分类标题不能为空'
+      }
+      if (errMsg) {
+        ctx.flash = { wraning: errMsg }
+        ctx.redirect('back')
+        return
+      }
+      const existed = await CategoryModel.findOne({ title })
+      console.log(existed);
+      if (existed) {
+        ctx.flash = { warning: '分类已存在' }
+        ctx.redirect('back')
+        return
+      }
+      const category = {
+        name,
+        title,
+        desc,
+      }
+      await CategoryModel.create(category)
+      ctx.flash = {
+        success: '新增分类成功'
+      }
+      ctx.redirect('/category')
+    }
+  },
+  async delete(ctx, next) {
+    const cid = ctx.params.id
+    if (cid.length !== 24) {
+      ctx.throw(404, '分类不存在')
+      return
+    }
+    const category = await CategoryModel.findById(cid)
+    if (!category) {
+      ctx.throw(404, '分类不存在')
+      return
+    }
+
+    await CategoryModel.findByIdAndRemove(cid)
+    ctx.flash = { success: '删除分类成功' }
+    ctx.redirect('back')
   }
 }
--- node/Bolg/routes/index.js
+++ node/Bolg/routes/index.js
@@ -1,4 +1,4 @@
-const router = require('koa-router')();
+const router = require('koa-router')()
 
 async function isLoginUser(ctx, next) {
   if (!ctx.session.user) {
@@ -17,33 +17,41 @@ async function isAdmin(ctx, next) {
     ctx.flash = { warning: '没有权限' }
     return ctx.redirect('back')
   }
-  await next();
+  await next()
 }
 
 module.exports = (app) => {
-  // 从数据库取文章数据
-  // router.get('/', require('./home').index);
-  router.get('/signup', require('./user').signup);
-  router.post('/signup', require('./user').signup);
-  router.get('/signin', require('./user').signin);
-  router.post('/signin', require('./user').signin);
-  router.post('/', require('./user').signin);
-  router.get('/signout', isLoginUser, require('./user').signout);
-  router.get('/posts/new', isLoginUser, require('./posts').create);
-
+  // router.get('/', require('./home').index)
+  router.get('/signup', require('./user').signup)
+  router.post('/signup', require('./user').signup)
+  router.get('/signin', require('./user').signin)
+  router.post('/signin', require('./user').signin)
+  router.get('/signout', isLoginUser, require('./user').signout)
+  // router.get('/about', require('./about').index)
+  router.get('/posts/new', isLoginUser, isAdmin, require('./posts').create)
   // 文章
-  router.get('/', require('./posts').index);
-  router.get('/posts', require('./posts').index);
-  router.post('/posts/new', isLoginUser, require('./posts').create);
-  router.get('/posts/:id', require('./posts').show);
-
+  router.get('/', require('./posts').index)
+  router.get('/posts', require('./posts').index)
+  router.post('/posts/new', isLoginUser, require('./posts').create)
+  router.get('/posts/:id', require('./posts').show)
+  // router.get('/posts/:id/edit', require('./posts').edit)
+  // router.post('/posts/:id/edit', require('./posts').edit)
+  // router.get('/posts/:id/delete', require('./posts').delete)
   // 评论
-  router.post('/comments/new', isLoginUser, require('./comments').create);
-  router.get('/comments/:id/delete', isLoginUser, require('./comments').delete);
-
+  router.post('/comments/new', isLoginUser, require('./comments').create)
+  router.get('/comments/:id/delete', isLoginUser, require('./comments').delete)
   // 分类
-  router.get('/category', require('./category').list);
+  router.get('/category', isAdmin, require('./category').list)
+  router.get('/category/new', require('./category').newCategory)
+  router.post('/category/new', require('./category').newCategory)
+
   app
     .use(router.routes())
     .use(router.allowedMethods())
+
+  // app.use(async (ctx, next) => {
+  //   await ctx.render('404', {
+  //     title: 'page not find'
+  //   })
+  // })
 }
