94b5f4ff7b6f01247ee431d6e114f086b1ada60f
10swan <1259496020@qq.com>
2022-01-14 18:12:33 +0800

    vue-elema

--- Vue/elema/src/views/goods/Goods.vue
+++ Vue/elema/src/views/goods/Goods.vue
@@ -4,7 +4,10 @@
       <ul>
         <li v-for="(item, index) in goods"
             :key="index"
-            class="menu-item">
+            class="menu-item"
+            @click="selectMenu(index)"
+            :class="{'current' : currentIndex === index}"    
+        >
           <span class="text">
             <support-ico v-show="item.type != -1"
               :size="3"
@@ -14,7 +17,32 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">商品</div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li ref="foodList" class="food-list" v-for="(item, index) in goods" :key="index">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li class="food-item" v-for="(food, idx) in item.foods" :key="idx">
+              <div class="icon">
+                <img :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -28,6 +56,7 @@ export default {
   data () {
     return {
       goods: [],
+      currentIndex:0
     };
   },
   created () {
@@ -45,6 +74,30 @@ export default {
       this.menuScroll = new BScroll(this.$refs.menuWrapper,{
         click: true
       })
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper,{
+        click:true,
+        probeType:3
+      })
+
+      this.foodsScroll.on('scroll', pos=>{
+        let listLen = this.$refs.foodList.length
+
+        console.log(Math.round(Math.abs(pos.y)));
+
+        for(let i=0;i<listLen;i++){
+          let cur = this.$refs.foodList[i];
+          // console.log(cur.offsetTop);
+          if(cur.offsetTop - Math.round(Math.abs(pos.y))  < 10 ){
+            this.currentIndex=i
+          }
+        }
+      })
+    },
+    selectMenu(index){
+      console.log(this.$refs.foodList);
+      let el = this.$refs.foodList[index];
+      this.foodsScroll.scrollToElement(el,300)
+      this.currentIndex = index;
     }
   }
 };
@@ -74,11 +127,61 @@ export default {
       line-height 14px
       align-items center
       font-size $fontsize-small
-
+      &.current
+        background-color #fff
+        font-weight: 700
   
-
   .foods-wrapper 
     flex: 1;
-  
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1;
+      font-size $fontsize-small
+      color rgb(147, 153, 159)
+      background $color-background-ssss
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      position: relative;
+      &:last-child
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+        img
+          width 100%
+      .content 
+        flex 1
+        .name 
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .desc, .extra 
+          line-height 10px
+          font-size 10px
+          color rgb(147, 153, 159)
+        .desc 
+          line-height 12px
+          margin-bottom 8px
+        .extra 
+          .count 
+            margin-right 12px
+        .price 
+          font-weight 700
+          line-height 24px
+          .now 
+            margin-right 8px
+            font-size 14px
+            color rgb(240, 20, 20)
+          .old 
+            text-decoration line-through
+            font-size 10px
+            color rgb(147, 153, 159)
+
 
 </style>
