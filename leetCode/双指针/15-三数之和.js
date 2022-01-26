bc80a564d61dfe021aab86cb2092885f57b61919
10swan <1259496020@qq.com>
2022-01-26 23:37:41 +0800

    三数之和

--- leetCode/双指针/15-三数之和.js
+++ leetCode/双指针/15-三数之和.js
@@ -2,39 +2,38 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-    let ans = [];
-    const length = nums.length;
-    // 当数组不存在或者长度小于3时，直接返回[]
-    if (!nums || length < 3) {
-        return ans;
+var threeSum = function (nums) {
+  // 不存在或者长度小于3直接返回空数组
+  if (!nums || nums.length < 3) return []
+  nums = nums.sort((a, b) => a - b)
+  let ans = []
+  let len = nums.length
+  // 第一个固定住的元素
+  for (let i = 0; i < len; i++) {
+    // 减枝，如果当前元素大于0，后面肯定大于0，停止循环；如果当前元素和上一个相同，跳过当前元素进入下一个防止重复计算
+    if (nums[i] > 0) break
+    if (nums[i - 1] === nums[i]) continue
+    let left = i + 1
+    let right = len - 1
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right]
+      if (sum === 0) {
+        ans.push([nums[i], nums[left], nums[right]])
+        while (left < right && nums[left] === nums[left + 1]) {
+          left++
+        }
+        while (left < right && nums[right] === nums[right--]) {
+          right--
+        }
+        left++
+        right--
+      } else if (sum > 0) {
+        right--
+      } else {
+        left++
+      }
     }
-    nums.sort((a, b) => a - b); //按照从小到大排序
-    for (let i = 0; i < length; i++) {
-        //当前数大于0，则三数之和一定大于0，直接退出循环返回ans
-        if (nums[i] > 0) {
-            break;
-        }
-        //当前数和前一个相同，去除重复元素进入下一次循环
-        if (nums[i - 1] === nums[i]) {
-            continue;
-        }
-        let left = i + 1; //左指针
-        let right = length - 1; //右指针
-        while (left < right) {
-            const sum = nums[i] + nums[left] + nums[right]; //三数之和
-            if (sum === 0) {
-                ans.push([nums[i], nums[left], nums[right]]);
-                while (left < right && nums[left] === nums[left + 1]) left++; //去重
-                while (left < right && nums[right] === nums[right - 1]) right--; //去重
-                left++;
-                right--;
-            } else if (sum > 0) {
-                right--;
-            } else {
-                left++;
-            }
-        }
-    }
-    return ans;
+  }
+  return ans
 };
+threeSum([-1,0,1,2,-1,-4])
