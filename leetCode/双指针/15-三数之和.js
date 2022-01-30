e05f26302dc911b493cd880edc67c1158736a2a1
10swan <1259496020@qq.com>
2022-01-30 23:40:33 +0800

    leetcode

--- leetCode/双指针/15-三数之和.js
+++ leetCode/双指针/15-三数之和.js
@@ -22,7 +22,7 @@ var threeSum = function (nums) {
         while (left < right && nums[left] === nums[left + 1]) {
           left++
         }
-        while (left < right && nums[right] === nums[right--]) {
+        while (left < right && nums[right] === nums[right-1]) {
           right--
         }
         left++
