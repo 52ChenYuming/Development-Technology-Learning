b49d7d9e5e345f09c56b3abc20fb69ae49e41905
10swan <1259496020@qq.com>
2022-02-14 20:56:43 +0800

    Vue3 basical

---
+++ Vue/Vue3/learn-vue3/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{state.count}}</h1>
+  <button @click="add">累加</button>
+</template>
+
+<script setup>
+
+import { reactive } from 'vue';
+
+const state = reactive({ 
+  count: 1
+ })
+
+function add() {
+  state.count++
+  console.log(state.count);
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
