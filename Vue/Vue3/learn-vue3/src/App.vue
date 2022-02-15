9fdebf808edc3bfe904d09cab0ffeec2c637f012
10swan <1259496020@qq.com>
2022-02-15 23:51:52 +0800

    vue3 动画

--- Vue/Vue3/learn-vue3/src/App.vue
+++ Vue/Vue3/learn-vue3/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <List></List>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/list.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    List
+}
 }
 </script>
 
--- Vue/Vue3/learn-vue3/src/components/list.vue
+++ Vue/Vue3/learn-vue3/src/components/list.vue
@@ -1,13 +1,19 @@
 <template>
-  list
+  <input type="text" v-model="title" @keydown.enter="addTodo">
+  <ul>
+    <li v-for="(todo,index) in todos" :key="index">{{todo}}</li>
+  </ul>
 </template>
 
 
 
-<script>
+<script setup>
+import { reactive, ref } from 'vue';
 
-export default {
-
+const title = ref('')
+const todos = reactive(['吃饭','睡觉'])
+function addTodo() {
+  todos.push(title.value)
 }
 
 </script>
