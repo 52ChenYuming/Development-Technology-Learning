638a17229c120d09affa000e50385dbf719a7943
10swan <1259496020@qq.com>
2022-03-24 22:29:34 +0800

    节流防抖

--- JavaScript/手写系列/节流/throttle.js
+++ JavaScript/手写系列/节流/throttle.js
@@ -20,53 +20,53 @@ function throttle(fn, delay = 1000) {
   };
 }
 
-// // 方法二：定时器
-// function throttle(fn, delay) {
-//   // 重置定时器
-//   let timer = null;
-//   // 返回闭包函数
-//   return function () {
-//     // 记录事件参数
-//     let args = arguments;
-//     // 如果定时器为空
-//     if (!timer) {
-//       // 开启定时器
-//       timer = setTimeout(() => {
-//         // 执行函数
-//         fn.apply(this, args);
-//         // 函数执行完毕后重置定时器
-//         timer = null;
-//       }, delay);
-//     }
-//   }
-// }
+// 方法二：定时器
+function throttle(fn, delay) {
+  // 重置定时器
+  let timer = null;
+  // 返回闭包函数
+  return function () {
+    // 记录事件参数
+    let args = arguments;
+    // 如果定时器为空
+    if (!timer) {
+      // 开启定时器
+      timer = setTimeout(() => {
+        // 执行函数
+        fn.apply(this, args);
+        // 函数执行完毕后重置定时器
+        timer = null;
+      }, delay);
+    }
+  }
+}
 
 // 方法三：时间戳 & 定时器
-// function throttle(fn, delay) {
-//   // 初始化定时器
-//   let timer = null;
-//   // 上一次调用时间
-//   let prev = null;
-//   // 返回闭包函数
-//   return function () {
-//     // 现在触发事件时间
-//     let now = Date.now();
-//     // 触发间隔是否大于delay
-//     let remaining = delay - (now - prev);
-//     // 保存事件参数
-//     const args = arguments;
-//     // 清除定时器
-//     clearTimeout(timer);
-//     // 如果间隔时间满足delay
-//     if (remaining <= 0) {
-//       // 调用fn，并且将现在的时间设置为上一次执行时间
-//       fn.apply(this, args);
-//       prev = Date.now();
-//     } else {
-//       // 否则，过了剩余时间执行最后一次fn
-//       timer = setTimeout(() => {
-//         fn.apply(this, args);
-//       }, delay);
-//     }
-//   };
-// }
+function throttle(fn, delay) {
+  // 初始化定时器
+  let timer = null;
+  // 上一次调用时间
+  let prev = null;
+  // 返回闭包函数
+  return function () {
+    // 现在触发事件时间
+    let now = Date.now();
+    // 触发间隔是否大于delay
+    let remaining = delay - (now - prev);
+    // 保存事件参数
+    const args = arguments;
+    // 清除定时器
+    clearTimeout(timer);
+    // 如果间隔时间满足delay
+    if (remaining <= 0) {
+      // 调用fn，并且将现在的时间设置为上一次执行时间
+      fn.apply(this, args);
+      prev = Date.now();
+    } else {
+      // 否则，过了剩余时间执行最后一次fn
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+      }, delay);
+    }
+  };
+}
