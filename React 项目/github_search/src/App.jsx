be2fdbaeb90bf0dacd12dd8aea4603a06f2afa17
10swan <1259496020@qq.com>
2022-07-02 20:11:47 +0800

    github seatch

---
+++ React 项目/github_search/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css'
+import List from './components/List'
+import Search from './components/Search'
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Search></Search>
+        <List></List>
+      </div>
+    )
+  }
+}
+
+
